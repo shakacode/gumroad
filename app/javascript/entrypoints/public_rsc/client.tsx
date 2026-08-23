aaff04c5db810339c9500c404c89b5aaa716e0a7
Ramez Weissa <ramez@shakacode.com>
2026-08-23 16:24:04 +0300

    Generate public RSC autobundled entries

--- app/javascript/entrypoints/public_rsc/client.tsx
+++ app/javascript/entrypoints/public_rsc/client.tsx
@@ -1,11 +1,7 @@
 import "react-on-rails-pro/registerDefaultRSCProvider/client";
-import registerServerComponent from "react-on-rails-pro/registerServerComponent/client";
 
 import BasePage from "$app/utils/base_page";
 import installBrowserTranslationGuard from "$app/utils/browser_translation_guard";
 
 installBrowserTranslationGuard();
 BasePage.initialize();
-registerServerComponent("ProductPage");
-registerServerComponent("ProfileRscCompatibilityPage");
-registerServerComponent("DiscoverPage");
