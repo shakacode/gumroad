16de2fff61d63fb311e2cb31a983fda8c9081615
Ramez Weissa <ramez@shakacode.com>
2026-09-02 14:50:45 +0300

    Add Discover result client foundation
    Reordered from existing PR #17.
    Source: 3777c09cc08381b093de0cb022bf9c00c836683c..79fbbfe92b46d98ca1cab6937ebaca5ee76ceb72

---
+++ app/javascript/components/Discover/DiscoverResults.client.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import * as React from "react";
+
+import DiscoverResultsCore from "$app/components/Discover/DiscoverResultsCore.client";
+
+export default function DiscoverResults() {
+  return <DiscoverResultsCore />;
+}
