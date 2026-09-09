7b5b9847f23192cf434352a3a76ab2b6c1d24ed4
Ramez Weissa <ramez@shakacode.com>
2026-09-09 18:58:15 +0300

    Add public RSC product data contract

---
+++ app/presenters/product_presenter/rsc_content_props.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class ProductPresenter::RscContentProps
+  CONTENT_KEYS = %i[
+    name seller collaborating_user ratings summary attributes description_html seller_reputation
+    duration_in_months free_trial is_compliance_blocked is_published native_type quantity_remaining streamable
+  ].freeze
+
+  def initialize(product_props:)
+    @product_props = product_props
+  end
+
+  def props
+    product_props.slice(*CONTENT_KEYS).merge(show_price: show_price?)
+  end
+
+  private
+    attr_reader :product_props
+
+    def show_price?
+      base_price_cents = if product_props[:bundle_products].present?
+        product_props[:bundle_products].sum { _1[:price] }
+      else
+        product_props[:price_cents]
+      end
+
+      product_props[:recurrences].nil? &&
+        product_props[:options].empty? &&
+        !product_props.dig(:rental, :rent_only) &&
+        (base_price_cents != 0 || product_props[:pwyw].present?)
+    end
+end
