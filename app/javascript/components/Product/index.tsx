49bbd570dda26929235c590cbf790b093965f23d
Ramez Weissa <ramez@shakacode.com>
2026-09-23 20:08:58 +0300

    Export the composed legacy product page

--- app/javascript/components/Product/index.tsx
+++ app/javascript/components/Product/index.tsx
@@ -1,11 +1,7 @@
-import * as React from "react";
-
-import { InteractiveProduct, type ProductData } from "$app/components/Product/Interactive";
-import { Product as LegacyProduct, legacyProductContent } from "$app/components/Product/LegacyProduct";
+import type { ProductData } from "$app/components/Product/Interactive";
+import { Product as LegacyProduct } from "$app/components/Product/LegacyProduct";
 
 export * from "$app/components/Product/Interactive";
 
 export type Product = ProductData;
-export const Product = ({ hideSellerByline, ...props }: React.ComponentProps<typeof LegacyProduct>) => (
-  <InteractiveProduct {...props} serverContent={legacyProductContent({ ...props, hideSellerByline })} />
-);
+export const Product = LegacyProduct;
